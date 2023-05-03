d69252878c2a440a4f70b12648198f6ce51ccd9f
Mota <mayconmotamendes@gmail.com>
2023-05-02 23:13:16 -0300

    add Chechmate

--- src/application/Application.java
+++ src/application/Application.java
@@ -17,7 +17,7 @@ public class Application {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -47,5 +47,7 @@ public class Application {
 
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
