26209a118c085629615d5fe04001f997ccf2ed87
Mota <mayconmotamendes@gmail.com>
2023-05-22 19:54:26 -0300

    att

--- src/application/Application.java
+++ src/application/Application.java
@@ -38,6 +38,15 @@ public class Application {
                 if (capturedPiece != null){
                     captured.add(capturedPiece);
                 }
+                if (chessMatch.getPromoted() !=  null){
+                    System.out.println("Enter piece for promotion (B/N/R/Q): ");
+                    String  type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+                        System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    chessMatch.replacePromotedPiece(type);
+                }
 
             }
             catch (ChessException | InputMismatchException e){
